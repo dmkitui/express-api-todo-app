19d778b3db0bb77d1c01ac9e15c8e4fa8320bf1b
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-05 17:57:17 +0300

    deleted

--- express-api-todo-app/todo-server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = require('./api/routes/index');
-const app = express();
-
-app.use('/', routes);
-
-app.listen(3000);
-console.log("To-do API server running at: http://localhost:3000");
