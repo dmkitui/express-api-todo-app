dd30a656438019d56ea1c56b783cc11d0683bb2b
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-05 17:56:48 +0300

    deleted

--- express-api-todo-app/api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.route('/').get((req, res) => {
-  res.send('This is the home route');
-});
-
-module.exports = router;
