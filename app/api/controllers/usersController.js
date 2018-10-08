559882d936da93cd9eec73b5b570c8f35d4636c5
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-08 14:50:42 +0300

    Add login and register routes

--- app/api/controllers/usersController.js
+++ app/api/controllers/usersController.js
@@ -11,10 +11,29 @@ const registerUser = function(req, res){
 			res.status(400);
 			res.json(error);
 		} else {
-			res.redirect('/login');
+			res.status(201);
+			res.json('Registration successful');
 		}
 	});
 };
 
-
+const loginUser = function(req, res, next) {
+	if(req.body.username && req.body.password) {
+		User.authenticate(req.body.username, req.body.password, function(error, user) {
+			if (error || !user) {
+				const error = new Error('Wrong username or password');
+				error.status = 401;
+				return next(error);
+			} else {
+				req.session.userId = user._id;
+				return res.json({message: 'Logg in successful'});
+			}
+		});
+	} else {
+		res.status(400);
+		res.json('Username and/or password is required');
+	}
+	
+}
 module.exports.registerUser = registerUser;
+module.exports.loginUser = loginUser;
